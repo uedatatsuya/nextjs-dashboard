6513939dcf26d9c9c6674ba619d84ceab3c1ecc6
tatsuya <tatsuya12151993@gmail.com>
2024-07-23 16:36:24 +0900

    tutorial

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,97 +1,93 @@
-import { sql } from '@vercel/postgres';
-import {
-  CustomerField,
-  CustomersTableType,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoiceRaw,
-  Revenue,
-} from './definitions';
-import { formatCurrency } from './utils';
+import { sql } from "@vercel/postgres";
+import type {
+	CustomerField,
+	CustomersTableType,
+	InvoiceForm,
+	InvoicesTable,
+	LatestInvoiceRaw,
+	Revenue,
+} from "./definitions";
+import { formatCurrency } from "./utils";
 
+// 収益データを取得する関数
 export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+	try {
+		// データベースから収益データを取得
+		const data = await sql<Revenue>`SELECT * FROM revenue`;
+		return data.rows;
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch revenue data.");
+	}
 }
 
+// 最新の請求書を取得する関数
 export async function fetchLatestInvoices() {
-  try {
-    const data = await sql<LatestInvoiceRaw>`
+	try {
+		// 最新の5件の請求書データを取得し、顧客情報と結合
+		const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.rows.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
-    return latestInvoices;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
-  }
+		// 金額をフォーマットして返す
+		const latestInvoices = data.rows.map((invoice) => ({
+			...invoice,
+			amount: formatCurrency(invoice.amount),
+		}));
+		return latestInvoices;
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch the latest invoices.");
+	}
 }
 
+// ダッシュボード用のカードデータを取得する関数
 export async function fetchCardData() {
-  try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
+	try {
+		// 複数のクエリを並列で実行
+		const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+		const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+		const invoiceStatusPromise = sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
-    ]);
+		const data = await Promise.all([
+			invoiceCountPromise,
+			customerCountPromise,
+			invoiceStatusPromise,
+		]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+		// 結果を整形して返す
+		const numberOfInvoices = Number(data[0].rows[0].count ?? "0");
+		const numberOfCustomers = Number(data[1].rows[0].count ?? "0");
+		const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? "0");
+		const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? "0");
 
-    return {
-      numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
-    };
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
-  }
+		return {
+			numberOfCustomers,
+			numberOfInvoices,
+			totalPaidInvoices,
+			totalPendingInvoices,
+		};
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch card data.");
+	}
 }
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
-  query: string,
-  currentPage: number,
+	query: string,
+	currentPage: number,
 ) {
-  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+	const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
-  try {
-    const invoices = await sql<InvoicesTable>`
+	try {
+		const invoices = await sql<InvoicesTable>`
       SELECT
         invoices.id,
         invoices.amount,
@@ -112,16 +108,16 @@ export async function fetchFilteredInvoices(
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
 
-    return invoices.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoices.');
-  }
+		return invoices.rows;
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch invoices.");
+	}
 }
 
 export async function fetchInvoicesPages(query: string) {
-  try {
-    const count = await sql`SELECT COUNT(*)
+	try {
+		const count = await sql`SELECT COUNT(*)
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE
@@ -132,42 +128,42 @@ export async function fetchInvoicesPages(query: string) {
       invoices.status ILIKE ${`%${query}%`}
   `;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch total number of invoices.');
-  }
+		const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+		return totalPages;
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch total number of invoices.");
+	}
 }
 
 export async function fetchInvoiceById(id: string) {
-  try {
-    const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
+	try {
+		const data = await sql<InvoiceForm>`
+		SELECT
+		invoices.id,
+		invoices.customer_id,
+		invoices.amount,
+		invoices.status
+		FROM invoices
+		WHERE invoices.id = ${id};
+		`;
 
-    const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
-
-    return invoice[0];
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
-  }
+		const invoice = data.rows.map((invoice) => ({
+			...invoice,
+			// Convert amount from cents to dollars
+			amount: invoice.amount / 100,
+		}));
+		console.log(invoice);
+		return invoice[0];
+	} catch (error) {
+		console.error("Database Error:", error);
+		throw new Error("Failed to fetch invoice.");
+	}
 }
 
 export async function fetchCustomers() {
-  try {
-    const data = await sql<CustomerField>`
+	try {
+		const data = await sql<CustomerField>`
       SELECT
         id,
         name
@@ -175,17 +171,19 @@ export async function fetchCustomers() {
       ORDER BY name ASC
     `;
 
-    const customers = data.rows;
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch all customers.');
-  }
+		const customers = data.rows;
+		return customers;
+	} catch (err) {
+		console.error("Database Error:", err);
+		throw new Error("Failed to fetch all customers.");
+	}
 }
 
+// フィルタリングされた顧客データを取得する関数
 export async function fetchFilteredCustomers(query: string) {
-  try {
-    const data = await sql<CustomersTableType>`
+	try {
+		// 顧客データを検索し、関連する請求書の情報も取得
+		const data = await sql<CustomersTableType>`
 		SELECT
 		  customers.id,
 		  customers.name,
@@ -203,15 +201,16 @@ export async function fetchFilteredCustomers(query: string) {
 		ORDER BY customers.name ASC
 	  `;
 
-    const customers = data.rows.map((customer) => ({
-      ...customer,
-      total_pending: formatCurrency(customer.total_pending),
-      total_paid: formatCurrency(customer.total_paid),
-    }));
+		 // 金額をフォーマットして返す
+		const customers = data.rows.map((customer) => ({
+			...customer,
+			total_pending: formatCurrency(customer.total_pending),
+			total_paid: formatCurrency(customer.total_paid),
+		}));
 
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch customer table.');
-  }
+		return customers;
+	} catch (err) {
+		console.error("Database Error:", err);
+		throw new Error("Failed to fetch customer table.");
+	}
 }
